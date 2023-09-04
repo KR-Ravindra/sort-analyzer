1269332b7e269c34d1276d5a0562cf465a916693
PallaviKhedle <pallavi8khedle@gmail.com>
2023-09-04 16:32:44 -0700

    ui changes

---
+++ ui/index.py
@@ -0,0 +1,53 @@
+# Import Module
+from tkinter import *
+import tkinter as tk
+from random import randint
+  
+
+# create root window
+root = Tk()
+
+
+# root window title and dimension
+root.title("Algorithm Efficiency Analyzer Tool")
+root.state('zoomed')
+
+input_var=tk.StringVar()
+
+# adding a label to the root window
+input_label = Label(root, text = 'Input', font=('calibre',10, 'bold'))
+  
+# creating a entry for input
+input_entry = Entry(root,textvariable = input_var, font=('calibre',10,'normal'))
+
+entry_random = str(randint(2, 10))
+def entryrandom():
+       input_entry.insert(END, entry_random)
+Button(root, text = "random", width = 10, height = 1, command = entryrandom).grid(row=5, column=2)
+  
+# function to display text when
+# button is clicked
+def clicked():
+	# input.configure(text = "I just got clicked")
+
+	input_entry=input_var.get()
+	print("The input  is : " + input_entry)
+	input_var.set("")
+
+
+
+# button widget with red color text
+# inside
+
+btn = Button(root, text = "Generate" ,
+			fg = "red", command=clicked)
+# set Button grid
+# btn.grid(column=1, row=0)
+input_label.grid(row=0,column=0)
+input_entry.grid(row=0,column=1)
+
+
+btn.grid(row=2,column=1)
+
+# Execute Tkinter
+root.mainloop()
