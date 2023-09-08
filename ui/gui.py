00d09270b3289c47a25e1c5d5806fac538f9a791
PallaviKhedle <pallavi8khedle@gmail.com>
2023-09-08 08:43:24 -0700

    code Refactoring

--- ui/gui.py
+++ ui/gui.py
@@ -1,10 +1,5 @@
 import customtkinter
-import os
-from PIL import Image
-import tkinter
-import tkinter as tk
 import random
-import re
 from CTkMessagebox import CTkMessagebox
 
 class App(customtkinter.CTk):
@@ -21,48 +16,40 @@ class App(customtkinter.CTk):
         self.grid_columnconfigure((2, 3), weight=0)
         self.grid_rowconfigure((0, 1, 2), weight=1)
 
-        # create sidebar frame with widgets
-        
-        self.sidebar_frame = customtkinter.CTkFrame(self, width=140, corner_radius=0)
-        self.sidebar_frame.grid(row=0, column=0, rowspan=4, sticky="nsew")
-        self.sidebar_frame.grid_rowconfigure(4, weight=1)
-        self.logo_label = customtkinter.CTkLabel(self.sidebar_frame, text="SortAnalyzer", font=customtkinter.CTkFont(size=20, weight="bold"))
+        # create Leftsidebar frame with widgets
+        self.left_sidebar_frame = customtkinter.CTkFrame(self, width=140, corner_radius=0)
+        self.left_sidebar_frame.grid(row=0, column=0, rowspan=4, sticky="nsew")
+        self.left_sidebar_frame.grid_rowconfigure(4, weight=1)
+        self.logo_label = customtkinter.CTkLabel(self.left_sidebar_frame, text="SortAnalyzer", font=customtkinter.CTkFont(size=20, weight="bold"))
         self.logo_label.grid(row=0, column=0, padx=20, pady=(20, 10))
-    
-        self.optionmenu_1 = customtkinter.CTkOptionMenu(self.sidebar_frame, dynamic_resizing=False,
+        self.optionmenu_1 = customtkinter.CTkOptionMenu(self.left_sidebar_frame, dynamic_resizing=False,
                                                       values=["Insertion Sort", "Bubble Sort", "Counting Sort","Heap Sort","Quick Sort","Merge Sort"])
         self.optionmenu_1.grid(row=1, column=0, padx=20, pady= 10)
-
-        self.entry = customtkinter.CTkEntry(self.sidebar_frame, placeholder_text="Enter Input", validate="key", validatecommand=(self.validate_command, '%P'))
+        self.entry = customtkinter.CTkEntry(self.left_sidebar_frame, placeholder_text="Enter Input")
         self.entry.grid(row=2, column=0,  padx=20, pady=10, sticky="nsew")
-        # Bind the <<Modified>> event to the entry widget
-        self.entry.bind("<<Modified>>", self.entry_modified)
-        self.sidebar_button_3 = customtkinter.CTkButton(self.sidebar_frame, command=self.generate_button_event, text="Generate")
-        self.sidebar_button_3.grid(row=3, column=0, padx=20, pady=10)
-        self.sidebar_button_1 = customtkinter.CTkButton(self.sidebar_frame, command=self.generate_random_array, text="Random Input")
-        self.sidebar_button_1.grid(row=4, column=0, padx=20, pady=10)
-        self.appearance_mode_label = customtkinter.CTkLabel(self.sidebar_frame, text="Appearance Mode:", anchor="w")
+        self.sorting_button = customtkinter.CTkButton(self.left_sidebar_frame, command=self.generate_button_event, text="Sorting")
+        self.sorting_button.grid(row=3, column=0, padx=20, pady=10)
+        self.random_input_btn = customtkinter.CTkButton(self.left_sidebar_frame, command=self.generate_random_array, text="Random Input")
+        self.random_input_btn.grid(row=4, column=0, padx=20, pady=10)
+        self.appearance_mode_label = customtkinter.CTkLabel(self.left_sidebar_frame, text="Appearance Mode:", anchor="w")
         self.appearance_mode_label.grid(row=5, column=0, padx=20, pady=(10, 0))
-        self.appearance_mode_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["Light", "Dark", "System"],
+        self.appearance_mode_optionemenu = customtkinter.CTkOptionMenu(self.left_sidebar_frame, values=["Light", "Dark", "System"],
                                                                        command=self.change_appearance_mode_event)
         self.appearance_mode_optionemenu.grid(row=6, column=0, padx=20, pady=(10, 10))
-        self.scaling_label = customtkinter.CTkLabel(self.sidebar_frame, text="UI Scaling:", anchor="w")
+        self.scaling_label = customtkinter.CTkLabel(self.left_sidebar_frame, text="UI Scaling:", anchor="w")
         self.scaling_label.grid(row=7, column=0, padx=20, pady=(10, 0))
-        self.scaling_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["80%", "90%", "100%", "110%", "120%"],
+        self.scaling_optionemenu = customtkinter.CTkOptionMenu(self.left_sidebar_frame, values=["80%", "90%", "100%", "110%", "120%"],
                                                                command=self.change_scaling_event)
         self.scaling_optionemenu.grid(row=8, column=0, padx=20, pady=(10, 20))
 
 
-        self.main_button_1 = customtkinter.CTkButton(master=self, fg_color="transparent", border_width=2, text_color=("gray10", "#DCE4EE"),text="QUIT", command=self.destroy_panel)
-        self.main_button_1.grid(row=3, column=1, columnspan=2, padx=(20, 20), pady=(20, 20), sticky="nsew")
+        self.quit_button = customtkinter.CTkButton(master=self, fg_color="transparent", border_width=2, text_color=("gray10", "#DCE4EE"),text="QUIT", command=self.destroy_panel)
+        self.quit_button.grid(row=3, column=1, columnspan=2, padx=(20, 20), pady=(20, 20), sticky="nsew")
 
         self.appearance_mode_optionemenu.set("Dark")
         self.scaling_optionemenu.set("100%")
-        self.optionmenu_1.set("Algorithms")
+        self.optionmenu_1.set("Select Algorithms")
 
-    def open_input_dialog_event(self):
-        dialog = customtkinter.CTkInputDialog(text="Type in a number:", title="CTkInputDialog")
-        print("CTkInputDialog:", dialog.get_input())
 
     def change_appearance_mode_event(self, new_appearance_mode: str):
         customtkinter.set_appearance_mode(new_appearance_mode)
@@ -71,23 +58,6 @@ class App(customtkinter.CTk):
         new_scaling_float = int(new_scaling.replace("%", "")) / 100
         customtkinter.set_widget_scaling(new_scaling_float)
 
-   
-
-    def validate_command(self,data):
-        
-        # If the input is empty or matches the pattern of comma-separated integers
-        if data == re.match(r'^(\d+(,\s*\d+)*)?$',str(data)):
-            return True
-        else:
-            print('Invalid Input')
-            return False
-        
-    def entry_modified(self, event):
-        # Check if the entry content is not empty
-        if self.entry.get():
-            self.sidebar_button_3.configure(state="normal")
-        else:
-            self.sidebar_button_3.configure(state="disabled")
 
     def generate_button_event(self):
       # Retrieve the value of input_var
@@ -118,7 +88,7 @@ class App(customtkinter.CTk):
                     from tkinter import ttk
                     from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
                     from plotter.visualizer import Visualizer
-                    
+
                     self.canvas_frame = customtkinter.CTkFrame(self, width=140, corner_radius=0)
                     self.canvas_frame.grid(row=0, column=1, rowspan=2, sticky="nsew")
                     self.canvas_frame.grid_rowconfigure(4, weight=1)
@@ -134,7 +104,6 @@ class App(customtkinter.CTk):
         self.entry.delete(0, "end") 
 
         
-
     def generate_random_array(self):
         # Generate an array of random numbers (e.g., 10 numbers between 1 and 100)
         random_array = [random.randint(1, 100) for _ in range(10)]
