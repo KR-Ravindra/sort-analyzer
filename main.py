e896fcbf0aff789c655a0155eb598bf4ab461b14
Raja Ravindra Kathi <ravindra.kathi@csu.fullerton.edu>
2023-09-16 09:11:14 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,6 @@ import tkinter as tk
 import time
 from ui.gui import App
 
-
-def centerWindow(width, height, root):  # Return 4 values needed to center Window
-    screen_width = root.winfo_screenwidth()  # Width of the screen
-    screen_height = root.winfo_screenheight() # Height of the screen     
-    x = (screen_width/2) - (width/2)
-    y = (screen_height/2) - (height/2)
-    return int(x), int(y)
-
 if __name__ == "__main__":
     """Invokes when this application is called; by defaults asks for user input of an array and sort algo,
     if not defaults to a predefined one and calls bubble sort
